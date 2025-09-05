058711af206feedf9d2956768aca0b6c27b867b5
Rafael Flores <flores.freak@gmail.com>
2025-09-05 13:29:14 -0700

    CoreUi-Pro Installed

--- invoice-app/src/App.tsx
+++ invoice-app/src/App.tsx
@@ -2,12 +2,48 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { CAccordion, CAccordionBody, CAccordionHeader, CAccordionItem } from '@coreui/react-pro';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <CAccordion activeItemKey={2}>
+        <CAccordionItem itemKey={1}>
+          <CAccordionHeader>Accordion Item #1</CAccordionHeader>
+          <CAccordionBody>
+            <strong>This is the first item&#39;s accordion body.</strong> It is hidden by default,
+            until the collapse plugin adds the appropriate classes that we use to style each element.
+            These classes control the overall appearance, as well as the showing and hiding via CSS
+            transitions. You can modify any of this with custom CSS or overriding our default
+            variables. It&#39;s also worth noting that just about any HTML can go within the{' '}
+            <code>.accordion-body</code>, though the transition does limit overflow.
+          </CAccordionBody>
+        </CAccordionItem>
+        <CAccordionItem itemKey={2}>
+          <CAccordionHeader>Accordion Item #2</CAccordionHeader>
+          <CAccordionBody>
+            <strong>This is the second item&#39;s accordion body.</strong> It is hidden by default,
+            until the collapse plugin adds the appropriate classes that we use to style each element.
+            These classes control the overall appearance, as well as the showing and hiding via CSS
+            transitions. You can modify any of this with custom CSS or overriding our default
+            variables. It&#39;s also worth noting that just about any HTML can go within the{' '}
+            <code>.accordion-body</code>, though the transition does limit overflow.
+          </CAccordionBody>
+        </CAccordionItem>
+        <CAccordionItem itemKey={3}>
+          <CAccordionHeader>Accordion Item #3</CAccordionHeader>
+          <CAccordionBody>
+            <strong>This is the second item&#39;s accordion body.</strong> It is hidden by default,
+            until the collapse plugin adds the appropriate classes that we use to style each element.
+            These classes control the overall appearance, as well as the showing and hiding via CSS
+            transitions. You can modify any of this with custom CSS or overriding our default
+            variables. It&#39;s also worth noting that just about any HTML can go within the{' '}
+            <code>.accordion-body</code>, though the transition does limit overflow.
+          </CAccordionBody>
+        </CAccordionItem>
+      </CAccordion>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
