46bcf3471ca6da34767843ea6986336b14d6e9eb
Rafael Flores <flores.freak@gmail.com>
2025-09-09 02:55:52 -0700

    InvoiceForm

--- invoice-app/src/pages/invoice-grid/Invoice-grid.tsx
+++ invoice-app/src/pages/invoice-grid/Invoice-grid.tsx
@@ -1,11 +1,36 @@
-import './Invoice-Grid.css'
+import { useState } from 'react';
+import './Invoice-Grid.css';
+import { AgGridReact } from 'ag-grid-react';
+import { AllCommunityModule, ModuleRegistry, type ColDef } from 'ag-grid-community'; 
+
+// Register all Community features
+ModuleRegistry.registerModules([AllCommunityModule]);
+
+interface IRow {
+  make: string;
+  model: string;
+  price: number;
+  electric: boolean;
+}
 
 function InvoiceGrid() {
+  const [rowData, setRowData] = useState<IRow[]>([
+    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+    { make: "Ford", model: "F-Series", price: 33850, electric: false },
+    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+  ]);
+
+  const [colDefs, setColDefs] = useState<ColDef<IRow>[]>([
+    { field: "make" },
+    { field: "model" },
+    { field: "price" },
+    { field: "electric" },
+  ]);
 
   return (
-    <>
-        <h1>Invoice Grid</h1>
-    </>
+    <div style={{ width: "100%", height: "500px" }}>
+      <AgGridReact rowData={rowData} columnDefs={colDefs} />
+    </div>
   )
 }
 
